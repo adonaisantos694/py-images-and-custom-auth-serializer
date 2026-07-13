baedf93adb007122b245a90bd927c020b1c08cb3
Adonai <adonaisoares694@gmail.com>
2026-07-13 20:05:50 -0300

    Implement solution 1.5

--- user/models.py
+++ user/models.py
@@ -1,10 +1,67 @@
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import (
+    AbstractUser,
+    BaseUserManager,
+)
 from django.db import models
+
+
+class UserManager(BaseUserManager):
+    use_in_migrations = True
+
+    def create_user(
+        self,
+        email,
+        password=None,
+        **extra_fields,
+    ):
+        if not email:
+            raise ValueError("Users must have an email address")
+
+        email = self.normalize_email(email)
+
+        user = self.model(
+            email=email,
+            **extra_fields,
+        )
+        user.set_password(password)
+        user.save(using=self._db)
+
+        return user
+
+    def create_superuser(
+        self,
+        email,
+        password=None,
+        **extra_fields,
+    ):
+        extra_fields.setdefault("is_staff", True)
+        extra_fields.setdefault("is_superuser", True)
+        extra_fields.setdefault("is_active", True)
+
+        if not extra_fields["is_staff"]:
+            raise ValueError(
+                "Superuser must have is_staff=True."
+            )
+
+        if not extra_fields["is_superuser"]:
+            raise ValueError(
+                "Superuser must have is_superuser=True."
+            )
+
+        return self.create_user(
+            email,
+            password,
+            **extra_fields,
+        )
 
 
 class User(AbstractUser):
     username = None
-    email = models.EmailField(unique=True)
+    email = models.EmailField(
+        unique=True,
+    )
 
     USERNAME_FIELD = "email"
     REQUIRED_FIELDS = []
+
+    objects = UserManager()
